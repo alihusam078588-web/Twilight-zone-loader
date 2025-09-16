6a1ecb749d5d5e2269cd4a6b6d9dbe2cd5db1afc
alihusam078588-web <alihusam078588@gmail.com>
2025-09-16 21:11:27 +0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,13 @@
 -- Twilight Zone Loader
-loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/twilight-zone-loader/main/script.lua"))()
+-- Created by Ali_hhjjj + ChatGPT
+
+local scriptUrl = "https://raw.githubusercontent.com/alihusam078588-web/Twilight-zone-loader/main/script.lua"
+local success, response = pcall(function()
+    return game:HttpGet(scriptUrl)
+end)
+
+if success then
+    loadstring(response)()
+else
+    warn("Failed to load Twilight Zone script. Error:", response)
+end
