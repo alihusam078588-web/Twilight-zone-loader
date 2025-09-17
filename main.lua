734a57e6baaf42c7128e63cae1d5b1f20b059881
alihusam078588-web <alihusam078588@gmail.com>
2025-09-17 08:19:51 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -1,142 +1,119 @@
--- Twilight Zone Hub (main.lua)
+-- Twilight Zone GUI Main Script
+-- Credits: Ali_hhjjj
+-- Tester/Helper: GoodJOBS3
+-- Special thanks: Olivia (creator of Riddance Hub, WindUI)
 
 -- Load WindUI
-local Library = loadstring(game:HttpGet("https://footagesus.github.io/WindUI-Docs/library.lua"))()
-local Window = Library:Window("Twilight Zone Hub", "by Ali_hhjjj", Enum.KeyCode.RightControl, 500, 350)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FootageSUS/WindUI/main/library.lua"))()
 
-----------------------------------------------------------------
--- ESP Tab
-----------------------------------------------------------------
-local espEnabled = false
+-- Create Window
+local Window = Library:CreateWindow({
+    Name = "Twilight Zone",
+    Themeable = {
+        Info = "Made by Ali_hhjjj"
+    }
+})
 
-local function toggleESP(state)
-    espEnabled = state
-    for _, v in pairs(game.Workspace:GetDescendants()) do
-        if v:IsA("Model") and v:FindFirstChild("Humanoid") then
-            if state then
-                if not v:FindFirstChild("ESPHighlight") then
-                    local hl = Instance.new("Highlight")
-                    hl.Name = "ESPHighlight"
-                    hl.Parent = v
-                    hl.FillColor = Color3.fromRGB(0, 255, 0)
-                end
-            else
-                if v:FindFirstChild("ESPHighlight") then
-                    v.ESPHighlight:Destroy()
-                end
-            end
-        end
-    end
-end
+-- Tabs
+local farmTab = Window:CreateTab("Auto Farm")
+local teleportTab = Window:CreateTab("Teleport")
+local espTab = Window:CreateTab("ESP")
+local miscTab = Window:CreateTab("Misc")
+local creditsTab = Window:CreateTab("Credits")
 
-local tabESP = Window:Tab("ESP", "rbxassetid://4483345998")
-tabESP:Toggle("Enable ESP", false, function(value)
-    toggleESP(value)
-end)
+----------------------------------------------------
+-- Auto Farm Tab
+----------------------------------------------------
+farmTab:CreateToggle({
+    Name = "Auto Farm Machines",
+    CurrentValue = false,
+    Flag = "AutoFarmMachines",
+    Callback = function(Value)
+        print("Auto Farm Machines:", Value)
+        -- TODO: Add your autofarm logic here
+    end,
+})
 
-----------------------------------------------------------------
+----------------------------------------------------
 -- Teleport Tab
-----------------------------------------------------------------
-local tabTP = Window:Tab("Teleport", "rbxassetid://4483345998")
+----------------------------------------------------
+teleportTab:CreateButton({
+    Name = "Teleport to Elevator",
+    Callback = function()
+        print("Teleporting to Elevator...")
+        -- TODO: Add teleport-to-elevator logic
+    end,
+})
 
-local player = game.Players.LocalPlayer
-local function tpTo(part)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = part.CFrame + Vector3.new(0, 3, 0)
-    end
-end
+teleportTab:CreateButton({
+    Name = "Teleport to Random Machine",
+    Callback = function()
+        print("Teleporting to random machine...")
+        -- TODO: Add teleport-to-random-machine logic
+    end,
+})
 
-tabTP:Button("Teleport to Elevator", function()
-    local elevator = workspace:FindFirstChild("Elevator")
-    if elevator then tpTo(elevator) end
-end)
+teleportTab:CreateToggle({
+    Name = "Auto Teleport to Machines",
+    CurrentValue = false,
+    Flag = "AutoTPMachines",
+    Callback = function(Value)
+        print("Auto Teleport to Machines:", Value)
+        -- TODO: Add auto teleport machine logic
+    end,
+})
 
-tabTP:Button("Teleport to Random Machine", function()
-    local gens = workspace.CurrentRoom and workspace.CurrentRoom:FindFirstChild("ProjectorMap") and workspace.CurrentRoom.ProjectorMap:FindFirstChild("Generators")
-    if gens then
-        local machines = gens:GetChildren()
-        if #machines > 0 then
-            tpTo(machines[math.random(1, #machines)])
-        end
-    end
-end)
+teleportTab:CreateToggle({
+    Name = "Auto Teleport to Elevator when done",
+    CurrentValue = false,
+    Flag = "AutoTPElevator",
+    Callback = function(Value)
+        print("Auto Teleport to Elevator:", Value)
+        -- TODO: Add auto teleport elevator logic
+    end,
+})
 
-local autoElevator = false
-tabTP:Toggle("Auto TP to Elevator", false, function(state)
-    autoElevator = state
-    if state then
-        task.spawn(function()
-            while autoElevator do
-                task.wait(2)
-                local gens = workspace.CurrentRoom and workspace.CurrentRoom:FindFirstChild("ProjectorMap") and workspace.CurrentRoom.ProjectorMap:FindFirstChild("Generators")
-                if gens then
-                    local allDone = true
-                    for _, g in pairs(gens:GetChildren()) do
-                        if g:FindFirstChild("Progress") and g.Progress.Value < 1 then
-                            allDone = false
-                            break
-                        end
-                    end
-                    if allDone then
-                        local elevator = workspace:FindFirstChild("Elevator")
-                        if elevator then
-                            tpTo(elevator)
-                            autoElevator = false
-                        end
-                    end
-                end
-            end
-        end)
-    end
-end):Description("Automatically teleport to the elevator when all machines are done")
+----------------------------------------------------
+-- ESP Tab
+----------------------------------------------------
+espTab:CreateToggle({
+    Name = "ESP Machines",
+    CurrentValue = false,
+    Flag = "ESPMachines",
+    Callback = function(Value)
+        print("ESP Machines:", Value)
+        -- TODO: Add ESP for machines
+    end,
+})
 
-local autoMachine = false
-tabTP:Toggle("Auto TP to Machine", false, function(state)
-    autoMachine = state
-    if state then
-        task.spawn(function()
-            while autoMachine do
-                task.wait(5)
-                local gens = workspace.CurrentRoom and workspace.CurrentRoom:FindFirstChild("ProjectorMap") and workspace.CurrentRoom.ProjectorMap:FindFirstChild("Generators")
-                if gens then
-                    for _, g in pairs(gens:GetChildren()) do
-                        if g:FindFirstChild("Progress") and g.Progress.Value < 1 then
-                            tpTo(g)
-                            break
-                        end
-                    end
-                end
-            end
-        end)
-    end
-end):Description("Automatically teleport you to unfinished machines")
+espTab:CreateToggle({
+    Name = "ESP Spirits",
+    CurrentValue = false,
+    Flag = "ESPSpirits",
+    Callback = function(Value)
+        print("ESP Spirits:", Value)
+        -- TODO: Add ESP for spirits
+    end,
+})
 
-----------------------------------------------------------------
--- Player Tab
-----------------------------------------------------------------
-local tabPlayer = Window:Tab("Player", "rbxassetid://4483345998")
+----------------------------------------------------
+-- Misc Tab
+----------------------------------------------------
+miscTab:CreateToggle({
+    Name = "Infinite Stamina",
+    CurrentValue = false,
+    Flag = "InfiniteStamina",
+    Callback = function(Value)
+        print("Infinite Stamina:", Value)
+        -- TODO: Add stamina logic here
+    end,
+})
 
-local infiniteStamina = false
-tabPlayer:Toggle("Infinite Stamina", false, function(state)
-    infiniteStamina = state
-    task.spawn(function()
-        while infiniteStamina do
-            task.wait(1)
-            local stats = player:FindFirstChild("PlayerScripts") and player.PlayerScripts:FindFirstChild("Stamina")
-            if stats then
-                local stamina = stats:FindFirstChild("Value")
-                if stamina then
-                    stamina.Value = stamina.MaxValue
-                end
-            end
-        end
-    end)
-end)
-
-----------------------------------------------------------------
+----------------------------------------------------
 -- Credits Tab
-----------------------------------------------------------------
-local tabCredits = Window:Tab("Credits", "rbxassetid://4483345998")
-tabCredits:Label("Script by Ali_hhjjj")
-tabCredits:Label("Tester / Bug Finder: GoodJOBS3")
-tabCredits:Label("Special Thanks: Olivia (Riddance Hub, WindUI)")
+----------------------------------------------------
+creditsTab:CreateLabel("Made by Ali_hhjjj")
+creditsTab:CreateLabel("Tester/Helper: GoodJOBS3")
+creditsTab:CreateLabel("Special thanks: Olivia (Riddance Hub, WindUI)")
+
+print("Twilight Zone main script loaded!")
