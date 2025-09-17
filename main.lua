9c4fdddbe271cf954e981a13824ecb5ece89c02a
alihusam078588-web <alihusam078588@gmail.com>
2025-09-17 11:04:17 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -1,35 +1,68 @@
--- Load the WindUI library
+-- Twilight Zone GUI (main.lua)
+-- Using WindUI
+
+-- Load WindUI
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/alihusam078588-web/Twilight-zone-loader/main/windui.lua"))()
 
--- Create a main window
+-- Create the main window
 local Window = Library:CreateWindow({
     Title = "Twilight Zone GUI",
     Center = true,
     AutoShow = true,
 })
 
--- Add a tab
-local Tab = Window:CreateTab({
+-- Create a main tab
+local MainTab = Window:CreateTab({
     Name = "Main",
-    Icon = "rbxassetid://10734950020" -- optional icon
+    Icon = "rbxassetid://10734950020" -- optional
 })
 
--- Add a section inside the tab
-local Section = Tab:CreateSection("Main Features")
+-- Create a section
+local MainSection = MainTab:CreateSection("Main Features")
 
--- Add a button
-Section:CreateButton({
+-- Add buttons
+MainSection:CreateButton({
     Name = "Fly",
     Callback = function()
-        print("Fly enabled")
+        print("Fly enabled!")
+        -- put fly script here
+    end
+})
+
+MainSection:CreateButton({
+    Name = "Noclip",
+    Callback = function()
+        print("Noclip enabled!")
+        -- put noclip script here
     end
 })
 
 -- Add a toggle
-Section:CreateToggle({
+MainSection:CreateToggle({
     Name = "ESP",
     Default = false,
-    Callback = function(value)
-        print("ESP is now", value)
+    Callback = function(state)
+        print("ESP:", state)
+        -- put ESP toggle code here
     end
 })
+
+-- Add a slider
+MainSection:CreateSlider({
+    Name = "WalkSpeed",
+    Min = 16,
+    Max = 100,
+    Default = 16,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end
+})
+
+-- Credits tab
+local CreditsTab = Window:CreateTab({
+    Name = "Credits",
+    Icon = "rbxassetid://6034509993"
+})
+
+local CreditsSection = CreditsTab:CreateSection("Made By")
+CreditsSection:CreateLabel("Ali_hhjjj")
