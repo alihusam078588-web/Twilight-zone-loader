e1a99ac81b02ae0397637b52a8e43694c41eacf3
alihusam078588-web <alihusam078588@gmail.com>
2025-09-17 08:28:49 +0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,8 @@
--- Twilight Zone Loader
--- Loads main.lua from GitHub repo
-
-local url = "https://raw.githubusercontent.com/alihusam078588-web/Twilight-zone-loader/main/main.lua"
-
-local success, err = pcall(function()
-    loadstring(game:HttpGet(url))()
+local ok, res = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/alihusam078588-web/Twilight-zone-loader/main/main.lua")
 end)
-
-if not success then
-    warn("[TZ Loader] Failed to load main.lua:", err)
-else
-    print("[TZ Loader] Successfully loaded Twilight Zone GUI!")
+if not ok or not res or res == "" then
+    warn("[TZ Loader] Failed to load main.lua: HTTP 404")
+    return
 end
+loadstring(res)()
