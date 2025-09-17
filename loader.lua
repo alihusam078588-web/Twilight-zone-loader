f9c6dcd0eb51534a3ed4c8c1d29bd8617559e995
alihusam078588-web <alihusam078588@gmail.com>
2025-09-17 08:30:48 +0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,10 @@
 local ok, res = pcall(function()
     return game:HttpGet("https://raw.githubusercontent.com/alihusam078588-web/Twilight-zone-loader/main/main.lua")
 end)
+
 if not ok or not res or res == "" then
     warn("[TZ Loader] Failed to load main.lua: HTTP 404")
     return
 end
+
 loadstring(res)()
