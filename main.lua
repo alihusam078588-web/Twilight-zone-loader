b79e0cb5ddf7e7991eb5983dbe8e237154e8c6cf
alihusam078588-web <alihusam078588@gmail.com>
2025-09-17 14:42:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,42 +1,39 @@
--- Load Rayfield Library
+-- Twilight Zone using Rayfield UI
+
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
--- Create your main window
 local Window = Rayfield:CreateWindow({
     Name = "Twilight Zone",
     LoadingTitle = "Twilight Zone Loader",
     LoadingSubtitle = "by Ali",
     ConfigurationSaving = {
        Enabled = true,
-       FolderName = "TwilightZone", -- Where configs will save
+       FolderName = "TwilightZone",
        FileName = "TZ_Config"
     },
     Discord = {
        Enabled = false,
-       Invite = "", -- your discord invite
+       Invite = "",
        RememberJoins = false
     },
-    KeySystem = false, -- turn on if you want key system
+    KeySystem = false,
 })
 
--- Create Tabs
-local MainTab = Window:CreateTab("Main", 4483362458) -- Roblox asset ID for icon
+local MainTab = Window:CreateTab("Main", 4483362458)
 local OtherTab = Window:CreateTab("Other", 4483362458)
 
--- Add a button
 MainTab:CreateButton({
     Name = "Test Button",
     Callback = function()
-        print("Button pressed!")
+        print("✅ Test Button pressed!")
     end,
 })
 
--- Add a toggle
 OtherTab:CreateToggle({
     Name = "Test Toggle",
     CurrentValue = false,
     Flag = "TestToggle",
     Callback = function(Value)
-        print("Toggle set to:", Value)
+        print("🔘 Test Toggle is now:", Value)
     end,
 })
