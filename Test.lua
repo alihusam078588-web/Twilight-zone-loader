d415eecbf228ba8576ec98a28c61269c8d70b345
alihusam078588-web <alihusam078588@gmail.com>
2025-11-04 13:52:04 +0300

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,11 +1,3 @@
--- // Services
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Workspace = game:GetService("Workspace")
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
-
--- // Util
 local function findRepresentativePart(model)
     if not model then return nil end
     if model:IsA("BasePart") then return model end
