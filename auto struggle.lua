86e6be46a6d099e4bd4978412c92cf7c8e5e9300
alihusam078588-web <alihusam078588@gmail.com>
2026-03-21 20:10:28 +0300

    Update auto struggle.lua

--- auto struggle.lua
+++ auto struggle.lua
@@ -1,45 +1,31 @@
-local Players = game:GetService("Players")
-local VIM = game:GetService("VirtualInputManager")
-local RunService = game:GetService("RunService")
+local UIS = game:GetService("UserInputService")
+local RS = game:GetService("ReplicatedStorage")
 
-local player = Players.LocalPlayer
-local gui = player:WaitForChild("PlayerGui")
+local remote = RS:WaitForChild("Events"):WaitForChild("TwistedSquirmGrab")
 
-local function tap(x, y)
-    pcall(function()
-        VIM:SendMouseButtonEvent(x, y, 0, true, game, 0)
-        VIM:SendMouseButtonEvent(x, y, 0, false, game, 0)
-    end)
-end
+local running = false
+local dir = "left"
 
-local function getCenter(guiObject)
-    local pos = guiObject.AbsolutePosition
-    local size = guiObject.AbsoluteSize
-    return pos.X + size.X/2, pos.Y + size.Y/2
-end
+remote.OnClientEvent:Connect(function(action)
+    if action == "GrabStart" then
+        running = true
+    elseif action == "GrabEnd" then
+        running = false
+    end
+end)
 
 task.spawn(function()
     while true do
-        local ui = gui:FindFirstChild("TwistedSquirmEscapeUI")
+        task.wait(0.06)
 
-        if ui then
-            local left = ui:FindFirstChild("LeftTouchZone")
-            local right = ui:FindFirstChild("RightTouchZone")
-
-            if left and right and left.Visible and right.Visible then
-                while left.Visible and right.Visible do
-                    
-                    local lx, ly = getCenter(left)
-                    local rx, ry = getCenter(right)
-
-                    tap(lx, ly)
-                    tap(rx, ry)
-
-                    task.wait(0.03)
-                end
+        if running then
+            if dir == "left" then
+                remote:FireServer("Struggle", "left")
+                dir = "right"
+            else
+                remote:FireServer("Struggle", "right")
+                dir = "left"
             end
         end
-
-        task.wait(0.1)
     end
 end)
